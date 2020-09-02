90581d30028f3bffc583457c071ccaf1ae6764c2
Levent Elibal <leventelibal@gmail.com>
2020-09-02 19:44:16 +0000

    add git java install

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,4 +1,16 @@
-node ('master'){
-    stage('init'){
-        sh 'ssh -i /root/jenkins-master_rsa root@68.183.116.191 yum install install epel-release -y '
+node  {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        
+        
+        stage ( 'Init') { 
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install epel-release -y'
+        }    
+    
+        stage("Install git"){
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install git -y'
+        }
+        stage("install Java"){
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install java-1.8.0-openjdk-devel -y'
+        }        
+    }
 }
